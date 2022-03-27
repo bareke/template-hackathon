7d8d1644108aa8bff57c8d2936c8842067ed016f
David García <jdavidgarcia28@gmail.com>
2022-03-27 18:15:56 -0500

    Apollo config added

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,27 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+
+import VueApollo from 'vue-apollo';
+import ApolloClient from 'apollo-boost';
+
+const apolloClient = new ApolloClient({
+  // You should use an absolute URL here
+  uri: 'https://api.graphcms.com/simple/v1/awesomeTalksClone',
+});
+
+Vue.use(VueApollo);
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  apolloProvider,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app');
