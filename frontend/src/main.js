816e2063dbb401be98ef0d4909032113b5474721
David García <jdavidgarcia28@gmail.com>
2022-03-31 21:42:56 -0500

    [FEATURE]: Clean project struture

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,24 +4,10 @@ import './registerServiceWorker';
 import router from './router';
 import store from './store';
 
-import VueApollo from 'vue-apollo';
-import ApolloClient from 'apollo-boost';
-
-const apolloClient = new ApolloClient({
-  // You should use an absolute URL here
-  uri: 'https://api.graphcms.com/simple/v1/awesomeTalksClone',
-});
-
-Vue.use(VueApollo);
-const apolloProvider = new VueApollo({
-  defaultClient: apolloClient,
-});
-
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  apolloProvider,
   render: (h) => h(App),
 }).$mount('#app');
