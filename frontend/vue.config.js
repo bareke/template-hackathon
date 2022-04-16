a555e61c19e055834a143d2bd0abeacdca3a3635
David García <jdavidgarcia28@gmail.com>
2022-04-16 15:53:24 -0500

    [FEATURE]: PWA Support

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  pwa: {
+    name: "Hackathon",
+    themeColor: "#f2f2f2",
+    msTileColor: "#000",
+    workboxPluginMode: "GenerateSW",
+    workboxOptions: {
+      skipWaiting: true,
+    },
+  },
 });
