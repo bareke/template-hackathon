eef3ba586468f6d5a9dad9ce974043343904ec7b
David García <jdavidgarcia28@gmail.com>
2022-04-16 17:43:07 -0500

    Rollback

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,13 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  pwa: {
-    name: "Hackathon",
-    themeColor: "#f2f2f2",
-    msTileColor: "#000",
-    workboxPluginMode: "GenerateSW",
-    workboxOptions: {
-      skipWaiting: true,
-    },
-  },
 });
